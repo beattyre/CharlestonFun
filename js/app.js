a21350fd54debdae3452634e606d0fb51117b2c5
beattyre <beattyre@miamioh.edu>
2017-05-17 23:31:06 -0400

    create app.js
    Copy over script for running map functions through leaflet from index.htm

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var map;
+function initmap() {
+		map = new L.map('map');
+		var osmTile = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+		var osmAttributes = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+		var osm = L.tileLayer(osmTile, {
+				minZoom: 8,
+				maxZoom: 20,
+				attribution: osmAttributes,
+    });
+		map.setView(new L.LatLng(32.8211, -79.9705), 11);
+		map.addLayer(osm);
+};
+
+initmap();
+	
+// Load Charleston Point Data
+
+$.getJSON("https://rawgit.com/beattyre/CharlestonFun/master/data/CharlestonDestinations.geojson", function(data){
+  var destinations =  L.geoJson(data, {
+		pointToLayer: function(feature, latlng){
+			var marker = L.marker(latlng);
+			marker.bindPopup('<b>Destination:</b> ' + feature.properties.NAME + '<br>' + '<b>Type:</b> ' + feature.properties.TYPE);
+			return marker;
+		}
+	}).addTo(map);
+var clusters = L.markerClusterGroup({
+  maxClusterRadius: 300,
+	showCoverageOnHover: false
+ });
+ clusters.addLayer(destinations);
+map.addLayer(clusters);
+		});
+	
